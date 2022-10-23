f3091c78185d78e1939f24cb3d4dd3d5afb3eba7
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-10-23 23:37:54 +0530

    learning

---
+++ js/danish.js
@@ -0,0 +1,26 @@
+var name ="Danish"
+let fav = "codding" 
+const age = 17
+console.log(name)
+console.log(fav)
+console.log(age)
+var name ="name"
+console.log(name)
+name = "name1"
+console.log(name)
+//let fav = "urdu"//
+fav = "science"
+console.log(fav)
+//age = age + 1//
+const intro = ("hi my name is " + name + " and my fav sub is " + fav + " and my age is " + age)
+console.log(intro)
+function fn(parameter1 , parameter2){ 
+    return parameter1 * 2 +  parameter2
+}
+const result = fn(2 , 3) 
+console.log(result)
+function emptyfn(){
+ return 1;
+}
+const result1 = emptyfn()
+console.log(result1)
