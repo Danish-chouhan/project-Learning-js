0822e6c63eb922d5f4b15338f9a4d43bd25af506
Danish-chouhan <111457988+Danish-chouhan@users.noreply.github.com>
2022-10-24 16:49:14 +0530

    Add files via upload

---
+++ danish js/index.js
@@ -0,0 +1,33 @@
+// {
+//     const a  = "danish"
+//     let b = 17
+//     var c = 10
+// }
+
+
+// function fn(){
+//     var fnv = 100
+// }
+
+// /* console.log(a)
+// console.log(b)*/
+// //console.log(fnv)//
+const arr = [1 , "htr" , true]
+ console.log(arr[0])
+ console.log(arr[1])
+ console.log(arr[2])
+ for(let i = 0 ; i < arr .length ; ++i){
+           console.log((i + 1) * 2)
+ }
+// //for(let d = 10 ; d > 0 ; d--){
+//     console.log(d)
+// //}
+
+console.log(arr.length)
+
+
+const arrey = [1 , "danish" , "10"]
+
+for(let a = 0 ; a < arr.length ; a++){
+    console.log(arrey[a])
+}
